8b3e247a96839c89e492dc32c41cf469f1fd15e1
Lalith <79350691+lalithkanth@users.noreply.github.com>
2024-07-15 22:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ def download_file():
         return jsonify({'status': 'error', 'message': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
